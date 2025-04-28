0c807992dd236352007d57f8c412f0263c2bc1f7
M4rYu5 <30922014+M4rYu5@users.noreply.github.com>
2025-04-28 10:36:15 +0300

    working test in main function

--- HaKey/main.cpp
+++ HaKey/main.cpp
@@ -1,9 +1,29 @@
 ﻿// TODO: Delete /lib/libevdev library. I don't think I'm gonna need it anymore.
 
+#include <memory>
+#include <iostream>
+
+
 import System;
+import Core;
+import KeyDispatcher;
 
 
-int main() {
-	System::LinuxKeyDispatcher d = new System::LinuxKeyDispatcher(nullptr);
-	d.Listen();
-}
+
+class Layer1 : public HaKey::Core::KeyChainHandler{
+    void OnKey(HaKey::Core::KeyEvent key, std::shared_ptr<HaKey::Core::KeyResult> result) override{
+        std::cout << key.key_code << " " << key.state  << "\n";
+    };
+};
+
+
+int main()
+{
+    HaKey::KeyDispatcher d;
+    std::unique_ptr<HaKey::Core::KeyChainHandler> l = std::make_unique<Layer1>();
+    d.Add(std::move(l));
+    d.Listen();
+};
+
+
+
